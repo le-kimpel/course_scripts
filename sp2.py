b70ed1d4f3173a4d4b10152797cb9463975e5da1
Lauren Kimpel <lekimpel@gmail.com>
2023-03-01 18:01:16 -0500

    initial commit

--- sp2.py
+++ sp2.py
@@ -4,4 +4,10 @@ import numpy
 Script for calculating the boundaries of some p-chains
 '''
 if __name__ == "__main__":
-    
+
+# first, process the data    
+with open("complexA.txt") as f1:
+    s1 = f1.read()
+
+with open("complexB.txt") as f2:
+    s2 = f2.read()
