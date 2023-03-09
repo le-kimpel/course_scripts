d924dd48530c9160b44c9d47eb438604e1174547
Lauren Kimpel <lekimpel@gmail.com>
2023-03-08 19:19:50 -0500

    parenthesis fix

--- mod6_problem3.py
+++ mod6_problem3.py
@@ -1,20 +1,28 @@
 import numpy as np
+import matplotlib.pyplot as plt 
+
 '''
 Script pertaining to Problem 3, parts (b) and (d)
 '''
 
 def calc_z(Z, X, Y):
+    if np.sum((X[:, 1] == y)) == 0:
+        return 0.5
     return np.sum((X[:, 1] == Y) & (X[:, 2] == Z)) / np.sum((X[:, 1] == Y))
 
+'''
+PART (B) 
+'''
+
 # generate uniform seed 
-unif = np.random.uniform(low = 0, high - 1, size=(1000,3))
+unif = np.random.uniform(low = 0, high = 1, size=(1000,3))
 
 # now generate a bunch of random samples
-x = np.where(seeds[:, 0] < 1/2, 1, 0)
-y = np.where(seeds[:, 1] < np.exp(4*x - 2) / (1 + np.exp(4*x - 2)), 1, 0)
-z = np.where(seeds[:, 2] < np.exp(2*(x + y) - 2) / (1 + np.exp(2*(x + y) - 2)), 1, 0)
+x = np.where(unif[:, 0] < 1/2, 1, 0)
+y = np.where(unif[:, 1] < np.exp(4*x - 2) / (1 + np.exp(4*x - 2)), 1, 0)
+z = np.where(unif[:, 2] < np.exp(2*(x + y) - 2) / (1 + np.exp(2*(x + y) - 2)), 1, 0)
 
-X = np.zeros((n, 3), dtype=int)
+X = np.zeros((1000, 3), dtype=int)
 X[:, 0] = x
 X[:, 1] = y
 X[:, 2] = z
@@ -22,7 +30,18 @@ X[:, 2] = z
 estimates = []
 
 # estimate P(Z = 1 | Y = 1) 
-for i in range(1,1000):
-    z_est = calc_z(X[i], 1, 1)
+for i in range(0,1000):
+    z_est = calc_z(1,X[:i], 1)
     estimates.append(z_est)
-    
+
+
+plt.scatter(np.arange(0,1000, 1, dtype=int), estimates)
+plt.ylabel('P(Z | Y = 1)')
+plt.xlabel('N')
+plt.show()
+
+
+'''
+PART (D) 
+'''
+
