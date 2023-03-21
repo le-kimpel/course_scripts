4844520a93d3a367f90817aa4e4026308d35d38d
Lauren Kimpel <lekimpel@gmail.com>
2023-03-20 21:08:33 -0400

    update skeleton code

--- sp2.py
+++ sp2.py
@@ -1,13 +1,31 @@
 import numpy
 
-'''
-Script for calculating the boundaries of some p-chains
-'''
+class SimplicialComplex:
+    '''
+    Python representation of a generic simplicial complex.
+    '''
+    def __init__(self, dimension):
+        self.dimension = dimension
+        self.pchains = {}
+        return
+
+def get_pchains(sc, p):
+    '''
+    Return the p-chains of dimension p
+    '''
+    return
+
+def build_complex(data):
+    '''
+    Build a simplicial complex from the ingested data
+    '''
+    return
+  
 if __name__ == "__main__":
 
-# first, process the data    
-with open("complexA.txt") as f1:
-    s1 = f1.read()
+    # first, process the data    
+    with open("complexA.txt") as f1:
+        s1 = f1.read()
 
-with open("complexB.txt") as f2:
-    s2 = f2.read()
+    with open("complexB.txt") as f2:
+        s2 = f2.read()
