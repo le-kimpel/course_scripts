1b116ed1fa08579a59a064e5a9840be4228d4078
Lauren Kimpel <lekimpel@gmail.com>
2023-04-06 08:37:05 -0400

    0 and 1 simplices

--- AppliedTopology/TDA.py
+++ AppliedTopology/TDA.py
@@ -13,6 +13,39 @@ For each of these distances:
 Get the points of distance d from one another; construct 0,1, and 2-simplices.
 '''''
 
+def get_simplex(data, d, dimension):
+    '''
+    From data in matrix, construct a simplex of a particular dimension; this is for each distance d. 
+    '''
+    # 0-simplices:
+    if (dimension == 0):
+        simplex = []
+        for i in range(0, len(data)):
+            for j in range(0,len(data)):
+                if (data[i][j] == d):
+                    simplex.append(i)
+                    simplex.append(j)
+    
+    # 1-simplices:
+    if (dimension == 1):
+        simplex = []
+        for i in range(0,len(data)):
+            for j in range(0, len(data)):
+                if (data[i][j] == d):
+                    simplex.append((i,j))
+    return list(set(simplex))
+
+def get_distances(data):
+    '''
+    Get the 1-dimensional vector of all possible distances from the dist. matrix.
+    '''
+    vec = []
+    for i in range(0,len(data)):
+        for j in range(0, len(data)):
+            vec.append(data[i][j])
+    vec = sorted(list(set(vec)))
+    return vec
+
 def rank_order(df):
     '''
     Rank order the metric distances from point to point. (vector to vector.)
@@ -28,4 +61,7 @@ def rank_order(df):
 if __name__ == "__main__":
     df1 = pd.read_csv("Data/CDHWdata_1.csv")
     print(df1)
-    rank_order(df1)
+    D1 = rank_order(df1)
+    distances = get_distances(D1)
+    A = get_simplex(D1, distances[1], 1)
+    print(A)
